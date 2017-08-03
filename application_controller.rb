ff57b29b560937e7e9dbc25a8f62d88ee4fac79a
Arianna <amoore@cristoreyny.org>
2017-08-03 14:20:47 +0000

    first try

--- application_controller.rb
+++ application_controller.rb
@@ -6,10 +6,15 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
-
-
-
-
-
-
-end
+  
+  post "/results" do
+    
+    @sender_name = param[:sender_name]
+    @receivers_name = param[:receivers_name]
+    @message = param[:message]
+    card_maker(@sender_name,@receivers_name,@message)
+    
+    erb :results
+    
+  end
+end 
---
+++ models/model.rb
@@ -0,0 +1,5 @@
+def card_maker(sender_name, receivers_name, message)
+   @sender_name = @sender_name.capitalize
+   @receivers_name = @receivers_name.capitalize
+   @message = @message.upcase
+end
